cac4986cc62796205c19248065e67cb7fb6f2741
Ingo Weseloh <it@exanto.de>
2015-08-21 08:36:03 +0200

    Loop setup

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -e
 cd ~
 # git clone git://github.com/novalis111/.my_rc.git ~/.my_rc
-[[ ! -L ~/.vim ]] && ln -s ~/.my_rc/.vim ~/
+for f in ".vim" ".vimrc" ".tmux.conf" ".bash_aliases" ".bash_profile"
+do
+    [[ ! -L ~/$f ]] && ln -s ~/.my_rc/$f ~/ || echo "$f exists, skipped"
+done
 [[ ! -d ~/.vim/bundle/vim-airline ]] && git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
-[[ ! -L ~/.vimrc ]] && ln -s ~/.my_rc/.vimrc ~/
-[[ ! -L ~/.tmux.conf ]] && ln -s ~/.my_rc/.tmux.conf ~/
-[[ ! -L ~/.bash_aliases ]] && ln -s ~/.my_rc/.bash_aliases ~/
-[[ ! -L ~/.bash_profile ]] && ln -s ~/.my_rc/.bash_profile ~/
 if ! grep -q '.my_bash' ~/.bashrc; then
     echo "[ -r ~/.my_rc/.my_bash ] && . ~/.my_rc/.my_bash" >> ~/.bashrc
 fi
