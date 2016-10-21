5d44841efd5d90da757cf98eaaedff2d42abaad0
Ingo Weseloh <it@exanto.de>
2016-10-21 14:43:37 +0200

    Add docker-compose to getTools

--- getTools.sh
+++ getTools.sh
@@ -15,3 +15,7 @@ chmod +x n98-magerun.phar
 if [ ! -f magerun ]; then
     ln -s n98-magerun.phar magerun
 fi
+
+# docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
